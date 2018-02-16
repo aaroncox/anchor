f13d2fe820e8e9696579202c12382900f799efa0
Aaron Cox <aaron.cox@greymass.com>
2018-02-16 03:54:50 -0500

    linting and logging

--- app/main.dev.js
+++ app/main.dev.js
@@ -1,8 +1,6 @@
 /* eslint global-require: 0, flowtype-errors/show-errors: 0 */
 
 import { app, BrowserWindow, crashReporter, Menu, Tray } from 'electron';
-
-
 import i18n from 'i18next';
 import Backend from 'i18next-sync-fs-backend';
 import sprintf from 'i18next-sprintf-postprocessor';
@@ -10,25 +8,53 @@ import LanguageDetector from 'i18next-electron-language-detector';
 import { reactI18nextModule } from 'react-i18next';
 
 const path = require('path');
-
-// logging to file via electron-log
 const log = require('electron-log');
 const util = require('util');
+
+const installExtensions = async () => {
+  const installer = require('electron-devtools-installer');
+  const forceDownload = !!process.env.UPGRADE_EXTENSIONS;
+  const extensions = [
+    'REACT_DEVELOPER_TOOLS',
+    'REDUX_DEVTOOLS'
+  ];
+
+  return Promise
+    .all(extensions.map(name => installer.default(installer[name], forceDownload)))
+    .catch(console.log);
+};
 
 let tray = null;
 let menu = null;
 let manager = null;
 let anchor = null;
 
+// logging to file via electron-log
 log.transports.console = (msg) => {
-  const text = util.format.apply(util, msg.data);
+  const text = util.format(...msg.data);
   if (menu && menu.webContents) {
     menu.webContents.executeJavaScript(`console.log("[${msg.date.toLocaleTimeString()} ${msg.level}] ${text}")`);
   }
 };
 
-log.info('anchor: initializing');
+log.info('app: initializing');
 
+if (process.env.NODE_ENV === 'production') {
+  const sourceMapSupport = require('source-map-support');
+  sourceMapSupport.install();
+}
+
+if (process.env.NODE_ENV === 'development' || process.env.DEBUG_PROD === 'true') {
+  require('electron-debug')();
+
+  const p = path.join(__dirname, '..', 'app', 'node_modules');
+  require('module').globalPaths.push(p);
+
+  // Log all messages
+  log.transports.file.level = 'info';
+}
+
+// crash reporter for failures (NYI)
 crashReporter.start({
   productName: 'Anchor',
   companyName: '',
@@ -36,6 +62,7 @@ crashReporter.start({
   uploadToServer: false
 });
 
+// localization provider
 i18n
   .use(Backend)
   .use(LanguageDetector)
@@ -52,7 +79,7 @@ i18n
     },
     backend: {
       loadPath: path.join(__dirname, '/locales/{{lng}}/{{ns}}.json'),
-      // addPath: path.join(__dirname,  '/locales/{{lng}}/{{ns}}.missing.json'),
+      addPath: path.join(__dirname, '/locales/{{lng}}/{{ns}}.missing.json'),
       jsonIndent: 2
     },
     overloadTranslationOptionHandler: sprintf.overloadTranslationOptionHandler,
@@ -60,43 +87,15 @@ i18n
       wait: true
     }
   });
-
 global.i18n = i18n;
 
+// Don't display the dock/task icon (NYI - display when a non always on top window is rendered)
 app.dock.hide();
 
-if (process.env.NODE_ENV === 'production') {
-  const sourceMapSupport = require('source-map-support');
-  sourceMapSupport.install();
-}
-
-if (process.env.NODE_ENV === 'development' || process.env.DEBUG_PROD === 'true') {
-  require('electron-debug')();
-
-  const p = path.join(__dirname, '..', 'app', 'node_modules');
-  require('module').globalPaths.push(p);
-
-  // Log all messages
-  log.transports.file.level = 'info';
-  log.info('development mode enabled');
-}
-
-const installExtensions = async () => {
-  const installer = require('electron-devtools-installer');
-  const forceDownload = !!process.env.UPGRADE_EXTENSIONS;
-  const extensions = [
-    'REACT_DEVELOPER_TOOLS',
-    'REDUX_DEVTOOLS'
-  ];
-
-  return Promise
-    .all(extensions.map(name => installer.default(installer[name], forceDownload)))
-    .catch(console.log);
-};
-
 app.on('ready', async () => {
-  log.info('anchor: ready');
+  log.info('app: ready');
   if (process.env.NODE_ENV === 'development' || process.env.DEBUG_PROD === 'true') {
+    log.info('development mode enabled');
     await installExtensions();
   }
   createTray();
@@ -109,17 +108,20 @@ app.on('open-url', (event, url) => {
   createAnchor(url);
 });
 app.on('window-all-closed', () => {
-  log.info('anchor: window-all-closed');
+  log.info('app: window-all-closed');
 });
-app.on('will-finish-launching', () => { log.info('anchor: will-finish-launching'); });
-app.on('before-quit', () => { log.info('anchor: before-quit'); });
-app.on('will-quit', () => { log.info('anchor: will-quit'); });
-app.on('quit', () => { log.info('anchor: quit'); });
+app.on('will-finish-launching', () => { log.info('app: will-finish-launching'); });
+app.on('before-quit', () => { log.info('app: before-quit'); });
+app.on('will-quit', () => { log.info('app: will-quit'); });
+app.on('quit', () => { log.info('app: quit'); });
 
 const createTray = () => {
   log.info('creating tray menu');
+
   const assetsDirectory = path.join(__dirname, 'assets');
+
   tray = new Tray(path.join(assetsDirectory, 'icon.png'));
+
   tray.on('click', (event) => {
     toggleWindow();
     if (process.defaultApp && event.ctrlKey) {
@@ -129,6 +131,7 @@ const createTray = () => {
       }
     }
   });
+
   tray.on('right-click', () => {
     const contextMenu = Menu.buildFromTemplate([
       {
@@ -151,6 +154,7 @@ const createTray = () => {
 
 const createMenu = () => {
   log.info('tray menu: creating');
+
   menu = new BrowserWindow({
     width: 300,
     height: 450,
@@ -179,6 +183,7 @@ const createMenu = () => {
 
 const createManager = () => {
   log.info('manager: creating');
+
   manager = new BrowserWindow({
     width: 960,
     height: 540,
@@ -204,7 +209,8 @@ const createManager = () => {
 };
 
 const createAnchor = (url = false) => {
-  log.info('anchor: creating');
+  log.info('anchor link: creating');
+
   anchor = new BrowserWindow({
     width: 640,
     height: 395,
@@ -223,6 +229,7 @@ const createAnchor = (url = false) => {
   if (url) {
     const parse = require('url-parse');
     const parsed = parse(url, true);
+
     if (parsed.host === 'sign') {
       const opsExp = /\/tx\/((?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?)/;
       const opsMatch = opsExp.exec(parsed.pathname);
@@ -235,16 +242,19 @@ const createAnchor = (url = false) => {
         [, meta] = metaMatch;
       }
     }
+
     if (ops && meta) {
+      log.info('anchor link: found');
+      log.info(ops);
       anchor.loadURL(`file://${path.join(__dirname, 'index.html')}#/anchor/${ops}/${meta}`);
     } else {
-      log.info('anchor link failed');
+      log.info('anchor link: failed');
       log.info(parsed);
     }
   }
 
   anchor.webContents.on('did-finish-load', () => {
-    log.info('anchor: loaded');
+    log.info('app: loaded');
     anchor.show();
     anchor.focus();
   });
