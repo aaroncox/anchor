d8040fe9bfcf336682787ebae0d87f0a98732edf
Aaron Cox <aaron.cox@greymass.com>
2018-03-07 20:48:19 -0500

    removed console.log

--- app/shared/actions/wallet.js
+++ app/shared/actions/wallet.js
@@ -3,7 +3,6 @@ import * as types from './types';
 
 export function setKey(password, account, type, key) {
   return (dispatch: () => void) => {
-    console.log('action', password, account, type, key)
     dispatch(send('setKey', password, account, type, key));
   };
 }
